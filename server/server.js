841cf613d5a4a20f84609ce0dd11238ec6d42e20
ameyadeloitte <asuvarna@deloitte.com>
2018-02-26 00:14:28 -0600

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,29 +17,17 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   //Send message to the individual user
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
 
   // Broadcasting - will send to all user except the socket itself
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined the chat',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the chat'));
 
   socket.on('createMessage', (message) =>{
     console.log('createMessage', message);
 
     //Socket.emit sends data to one connection
     //io.emit sends data to every connection
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
---
+++ server/utils/message.test.js
@@ -0,0 +1,12 @@
+var expect = require('expect');
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate the correct message object', () => {
+    var from = 'admin';
+    var text = 'test message';
+    var message = generateMessage(from, text);
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({from, text});
+  });
+});
