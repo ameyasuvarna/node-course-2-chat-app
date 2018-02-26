b567d44ec691559450439f49dcc4f8a31cdfb717
ameyadeloitte <asuvarna@deloitte.com>
2018-02-25 19:21:00 -0600

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,14 +15,21 @@ app.use(express.static(publicPath)); //Serve up the Public folder
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Ameya',
-    text: 'Hey. What is going on.',
-    createdAt: 123
-  });
+  // socket.emit('newMessage', {
+  //   from: 'Ameya',
+  //   text: 'Hey. What is going on.',
+  //   createdAt: 123
+  // });
 
   socket.on('createMessage', (message) =>{
     console.log('createMessage', message);
+    //Socket.emit sends data to one connection
+    //io.emit sends data to every connection
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
