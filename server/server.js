2415e6b1eec987c630c3d4516bdcbb3852bb1452
ameyadeloitte <asuvarna@deloitte.com>
2018-03-18 17:08:25 -0500

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 const {generateMessage} = require('./utils/message');
 const {generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -11,17 +13,43 @@ const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath)); //Serve up the Public folder
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  //Send message to the individual user
-  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+  // //Send message to the individual user
+  // socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+  //
+  // // Broadcasting - will send to all user except the socket itself
+  // socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the chat'));
 
-  // Broadcasting - will send to all user except the socket itself
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the chat'));
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required');
+    }
+
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
+    // socket.leave('The Office Fans');
+
+    // io.emit -> io.to('The Office Fans').emit
+    // socket.broadcast.emit -> socket.boradcast.to('The Office Fans').emit
+    // socket.emit
+
+    //Send message to the individual user
+    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+
+    // Broadcasting - will send to all user except the socket itself
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) =>{
     console.log('createMessage', message);
@@ -38,7 +66,15 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
+
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+    }
   });
+
 });
 
 server.listen(port, () => {
