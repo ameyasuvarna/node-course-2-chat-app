3a4a05a5a236b6beffa4f754eb1a9240ec751ead
ameyadeloitte <asuvarna@deloitte.com>
2018-03-18 17:24:48 -0500

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -56,12 +56,21 @@ io.on('connection', (socket) => {
 
     //Socket.emit sends data to one connection
     //io.emit sends data to every connection
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
     callback(); //Send acknowledgement
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
+
+    //io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
